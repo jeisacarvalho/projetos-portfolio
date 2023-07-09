e426839811adcf99caaeee6bda80367982bccedb
jeisacarvalho <jeisasilva1823@gmail.com>
2023-07-09 06:34:43 -0300

    api

--- movies_lib/src/pages/Home.jsx
+++ movies_lib/src/pages/Home.jsx
@@ -4,7 +4,21 @@ const moviesURL = import.meta.env.VITE_API;
 const apiKey = import.meta.env.VITE_API_KEY;
 
 const Home = () => {
-  console.log(moviesURL, apiKey);
+ const [topMovies, settopMovies] = useState([]);
+
+  const getTopRatedMovies = async (url) => {
+    const res = await fetch(url);
+    const data = await res.json();
+
+    console.log(data)
+  };
+
+  useEffect(() => {
+    const topRatedUrl = `${moviesURL}top_rated?${apiKey}`;
+
+    getTopRatedMovies(topRatedUrl);
+
+  }, []);
   return (
     <div>
       <h1>Home</h1>
