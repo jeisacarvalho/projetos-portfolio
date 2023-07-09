7046f2694e5bbb3c9b339015d2947bf156e42ffb
jeisacarvalho <jeisasilva1823@gmail.com>
2023-07-09 07:13:21 -0300

    projeto

--- movies_lib/src/App.jsx
+++ movies_lib/src/App.jsx
@@ -7,8 +7,6 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-     
-    <h1>Movie Library</h1>
     <Outlet />
     </div>
       
--- movies_lib/src/pages/Home.jsx
+++ movies_lib/src/pages/Home.jsx
@@ -1,31 +1,33 @@
-import { useState, useEffect } from 'react';
-
+import { useEffect, useState } from "react";
 const moviesURL = import.meta.env.VITE_API;
 const apiKey = import.meta.env.VITE_API_KEY;
 
 const Home = () => {
- const [topMovies, settopMovies] = useState([]);
+  const [topMovies, setTopMovies] = useState([]);
 
   const getTopRatedMovies = async (url) => {
     const res = await fetch(url);
     const data = await res.json();
-
-    console.log(data)
+    setTopMovies(data.results);
   };
 
   useEffect(() => {
     const topRatedUrl = `${moviesURL}top_rated?${apiKey}`;
-
+    console.log(topRatedUrl);
     getTopRatedMovies(topRatedUrl);
-
   }, []);
+
+  console.log(topMovies);
+
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="container">
+      <h2 className="title">Melhores filmes:</h2>
+      <div className="movies-container">
+        {topMovies.length > 0 &&
+          topMovies.map((movie) => <MovieCard key={movie.id} movie={movie} />)}
+      </div>
     </div>
   );
 };
 
 export default Home;
-
- 
